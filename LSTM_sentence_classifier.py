889458a18af082960f121321566afc8244f3c8a5
yuchenlin <yuchenlin@sjtu.edu.cn>
2017-07-03 20:18:10 +0800

    simplify

--- LSTM_sentence_classifier.py
+++ LSTM_sentence_classifier.py
@@ -12,22 +12,12 @@ class LSTMClassifier(nn.Module):
     def __init__(self, embedding_dim, hidden_dim, vocab_size, label_size):
         super(LSTMClassifier, self).__init__()
         self.hidden_dim = hidden_dim
-
         self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
-
-        # The LSTM takes word embeddings as inputs, and outputs hidden states
-        # with dimensionality hidden_dim.
         self.lstm = nn.LSTM(embedding_dim, hidden_dim)
-
-        # The linear layer that maps from hidden state space to tag space
         self.hidden2label = nn.Linear(hidden_dim, label_size)
         self.hidden = self.init_hidden()
 
     def init_hidden(self):
-        # Before we've done anything, we dont have any hidden state.
-        # Refer to the Pytorch documentation to see exactly
-        # why they have this dimensionality.
-        # The axes semantics are (num_layers, minibatch_size, hidden_dim)
         # the first is the hidden h
         # the second is the cell  c
         return (autograd.Variable(torch.zeros(1, 1, self.hidden_dim)),
@@ -53,7 +43,8 @@ def train():
     HIDDEN_DIM = 6
     EPOCH = 5
 
-    model = LSTMClassifier(embedding_dim=EMBEDDING_DIM,hidden_dim=HIDDEN_DIM,vocab_size=len(word_to_ix),label_size=len(label_to_ix))
+    model = LSTMClassifier(embedding_dim=EMBEDDING_DIM,hidden_dim=HIDDEN_DIM,
+                           vocab_size=len(word_to_ix),label_size=len(label_to_ix))
     loss_function = nn.NLLLoss()
     optimizer = optim.Adam(model.parameters(),lr = 1e-3)
 
@@ -61,17 +52,10 @@ def train():
         avg_loss = 0.0
         count = 0
         for sent, label in training_data:
-            # data = lines.split('\t')[2]
-            # data = normalizeString(data).strip()
-            # input_data = [obj1.word_to_idx[word] for word in data.split(' ')]
-            # if len(input_data) > max_sequence_len:
-            #     input_data = input_data[0:max_sequence_len]
 
             # Also, we need to clear out the hidden state of the LSTM,
             # detaching it from its history on the last instance.
             model.hidden = model.init_hidden()
-
-            # input_data = Variable(torch.LongTensor(input_data))
             sent = data_loader.prepare_sequence(sent, word_to_ix)
             label = data_loader.prepare_label(label, label_to_ix)
             pred = model(sent)
@@ -80,13 +64,13 @@ def train():
             avg_loss += loss.data[0]
 
             count+=1
-            # if count % 500 == 0:
             print('epoch :%d iterations :%d loss :%g' % (i, count, loss.data[0]))
 
             loss.backward()
             optimizer.step()
         # torch.save(model.state_dict(), 'model' + str(i + 1) + '.pth')
-        print('the average loss after completion of %d epochs is %g' % ((i + 1), (avg_loss / len(training_data))))
+        print('the average loss after completion of %d epochs is %g'
+              % ((i + 1), (avg_loss / len(training_data))))
 
 
 train()
